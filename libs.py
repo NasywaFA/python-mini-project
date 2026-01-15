72c78b47d2b2d606e0739f4dedf56f69e6021ffb
NasywaFA <nasywa.school7@gmail.com>
2026-01-15 13:43:56 +0700

    add autoclicker

--- libs.py
+++
@@ -1,5 +0,0 @@
-def welcome_message(title):
-    style = '*' * (len(title) + 8)
-    print(style)
-    print(f'**| {title} |**')
-    print(style)
--- test.py
+++
@@ -1,4 +0,0 @@
-start = 0
-while start < 100:
-    print('CAPJAY KECAP')
-    start = start + 1
